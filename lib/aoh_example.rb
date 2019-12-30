8bda691d096cdd3bac8dba0ad43b27c6cac61fb9
Miwha Geschwind <miwhag@gmail.com>
2019-12-30 20:23:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,12 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
 end
 
-residents = [ 
+residents = [
+  { name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
 
 def literal_aoh
